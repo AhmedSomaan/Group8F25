50fd95b4294e6ec61e7e2adb194af8fec526cd94
Ahmed Somaan <ahmed_somaan@hotmail.com>
2025-11-28 17:33:50 -0500

    added functions declaration for managing data from database using mvc pattern

--- drivetracker/database/supabase.js
+++ drivetracker/database/supabase.js
@@ -1,1 +1,26 @@
 import { supabase } from "./supabaseClient";
+
+function ensureConfigured() {
+  if (!supabase || !supabase.auth) {
+    console.warn("Supabase client not configured");
+    return false;
+  }
+  return true;
+}
+
+export async function getOrCreateDemoUser() {
+
+}
+
+export async function getTripsForUser(userId) {
+
+}
+
+export async function insertTrip(userId) {
+
+}
+
+export async function insertScore(score) {
+
+}
+
